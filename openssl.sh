04ba6042f9e5b81e06adce1d6e919c7dbc67e5cc
Brian Shirai <brixen@gmail.com>
2015-12-26 23:20:22 -0800

    Try to use a local file.

--- openssl.sh
+++ openssl.sh
@@ -4,6 +4,7 @@ if [[ $TRAVIS_OS_NAME == osx ]]; then
   brew --prefix openssl
   `brew --prefix openssl`/bin/openssl version
 
-  gem install rubysl-openssl -v 2.7.0
+  curl -L -o rubysl-openssl-2.7.0.gem https://rubygems.org/downloads/rubysl-openssl-2.7.0.gem
+  gem install rubysl-openssl-2.7.0.gem
   gem uninstall rubysl-openssl -i $(rbx -e 'puts Rubinius::GEMS_PATH') -v 2.6.0
 fi
