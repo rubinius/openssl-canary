4826ee2a376f1a3a3883b558c54d98a53c957289
Brian Shirai <brixen@gmail.com>
2015-12-26 23:32:26 -0800

    Use install flags.

--- openssl.sh
+++ openssl.sh
@@ -5,6 +5,7 @@ if [[ $TRAVIS_OS_NAME == osx ]]; then
   `brew --prefix openssl`/bin/openssl version
 
   curl -L -o rubysl-openssl-2.7.0.gem https://rubygems.org/downloads/rubysl-openssl-2.7.0.gem
-  gem install rubysl-openssl-2.7.0.gem
+  gem install rubysl-openssl-2.7.0.gem -- --with-cppflags="-I$(brew --prefix openssl)/include" --with-ldflags="-L$(brew --prefix openssl)/lib"
   gem uninstall rubysl-openssl -i $(rbx -e 'puts Rubinius::GEMS_PATH') -v 2.6.0
+  gem install mspec
 fi
